20ed49c93fbb05aab6af829f1d22579fc6dca867
Jason Rowland <jsonxr@gmail.com>
2024-11-01 17:31:39 -0500

    sdl: refactor a bit

--- zig/sdl-example/src/main.zig
+++ zig/sdl-example/src/main.zig
@@ -2,42 +2,18 @@ const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 
+const Window = @import("window.zig").Window;
+
+pub fn example() !void {
+    const window = Window.init(.{
+        .title = "My Window",
+        .fullscreen = true,
+    });
+
+    try window.start();
+    defer window.deinit();
+}
+
 pub fn main() !void {
-    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
-        c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
-        return error.SDLInitializationFailed;
-    }
-    defer c.SDL_Quit();
-
-    const screen = c.SDL_CreateWindow("My Game Window", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 400, 140, c.SDL_WINDOW_OPENGL) orelse
-        {
-        c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
-        return error.SDLInitializationFailed;
-    };
-    defer c.SDL_DestroyWindow(screen);
-
-    const renderer = c.SDL_CreateRenderer(screen, -1, 0) orelse {
-        c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
-        return error.SDLInitializationFailed;
-    };
-    defer c.SDL_DestroyRenderer(renderer);
-
-    var quit = false;
-
-    while (!quit) {
-        var event: c.SDL_Event = undefined;
-        while (c.SDL_PollEvent(&event) != 0) {
-            switch (event.type) {
-                c.SDL_QUIT => {
-                    quit = true;
-                },
-                else => {},
-            }
-        }
-
-        _ = c.SDL_RenderClear(renderer);
-        c.SDL_RenderPresent(renderer);
-
-        c.SDL_Delay(10);
-    }
+    try example();
 }

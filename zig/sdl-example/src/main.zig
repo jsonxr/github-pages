1dd970cf8321b3030b8ac598a2b49d7a453537de
Jason Rowland <jsonxr@gmail.com>
2024-11-03 16:54:59 -0600

    remove fullscreen flag

--- zig/sdl-example/src/main.zig
+++ zig/sdl-example/src/main.zig
@@ -7,7 +7,6 @@ const Window = @import("window.zig").Window;
 pub fn example() !void {
     const window = Window.init(.{
         .title = "My Window",
-        .fullscreen = true,
     });
 
     try window.start();
